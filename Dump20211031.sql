8e212c9f31b3c5268c0ebca4044ef323a5ce5856
joseeduardob <76182948+joseeduardob@users.noreply.github.com>
2021-10-31 22:19:56 -0600

    Base de datos Actualizada 31/10

---
+++ Dump20211031.sql
@@ -0,0 +1,270 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: clinica
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alergias`
+--
+
+DROP TABLE IF EXISTS `alergias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alergias` (
+  `IdAlergias` int NOT NULL AUTO_INCREMENT,
+  `TipoAlergia` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdAlergias`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alergias`
+--
+
+LOCK TABLES `alergias` WRITE;
+/*!40000 ALTER TABLE `alergias` DISABLE KEYS */;
+INSERT INTO `alergias` VALUES (1,'Polvo'),(2,'Rash'),(5,'Mariscos');
+/*!40000 ALTER TABLE `alergias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `citas`
+--
+
+DROP TABLE IF EXISTS `citas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `citas` (
+  `IdCita` int NOT NULL AUTO_INCREMENT,
+  `IdPaciente` int NOT NULL,
+  `IdMedico` int NOT NULL,
+  `IdConsultorio` int NOT NULL,
+  `IdHorario` int NOT NULL,
+  PRIMARY KEY (`IdCita`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `citas`
+--
+
+LOCK TABLES `citas` WRITE;
+/*!40000 ALTER TABLE `citas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `citas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `consultorio`
+--
+
+DROP TABLE IF EXISTS `consultorio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `consultorio` (
+  `IdConsultorio` int NOT NULL AUTO_INCREMENT,
+  `Consultorio` varchar(45) NOT NULL,
+  `IdMedico` int NOT NULL,
+  PRIMARY KEY (`IdConsultorio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `consultorio`
+--
+
+LOCK TABLES `consultorio` WRITE;
+/*!40000 ALTER TABLE `consultorio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `consultorio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `especialidad`
+--
+
+DROP TABLE IF EXISTS `especialidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `especialidad` (
+  `IdEspecialidad` int NOT NULL AUTO_INCREMENT,
+  `Especialidad` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdEspecialidad`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especialidad`
+--
+
+LOCK TABLES `especialidad` WRITE;
+/*!40000 ALTER TABLE `especialidad` DISABLE KEYS */;
+INSERT INTO `especialidad` VALUES (1,'Maxilofacial'),(2,'Ortodoncista'),(3,'gg'),(4,'Anesteciologo');
+/*!40000 ALTER TABLE `especialidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `horario`
+--
+
+DROP TABLE IF EXISTS `horario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `horario` (
+  `IdHorario` int NOT NULL AUTO_INCREMENT,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  PRIMARY KEY (`IdHorario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `horario`
+--
+
+LOCK TABLES `horario` WRITE;
+/*!40000 ALTER TABLE `horario` DISABLE KEYS */;
+INSERT INTO `horario` VALUES (1,'2003-10-21','10:30:00'),(2,'2021-10-30','13:30:00');
+/*!40000 ALTER TABLE `horario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medico`
+--
+
+DROP TABLE IF EXISTS `medico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `medico` (
+  `idMedico` int NOT NULL AUTO_INCREMENT,
+  `PrimNom` varchar(45) NOT NULL,
+  `SegNom` varchar(45) NOT NULL,
+  `PrimApe` varchar(45) NOT NULL,
+  `SegApe` varchar(45) NOT NULL,
+  `Correo` varchar(45) NOT NULL,
+  `Direccion` varchar(45) NOT NULL,
+  `IdAlergias` int NOT NULL,
+  `IdTipoSangre` int NOT NULL,
+  `IdEspecialidad` int NOT NULL,
+  `DUI` int NOT NULL,
+  `ContEm` varchar(45) NOT NULL,
+  `IdHorario` int NOT NULL,
+  `FechaNac` datetime NOT NULL,
+  `Edad` int NOT NULL,
+  PRIMARY KEY (`idMedico`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medico`
+--
+
+LOCK TABLES `medico` WRITE;
+/*!40000 ALTER TABLE `medico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paciente`
+--
+
+DROP TABLE IF EXISTS `paciente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paciente` (
+  `idPaciente` int NOT NULL AUTO_INCREMENT,
+  `PrimNom` varchar(45) NOT NULL,
+  `SegNom` varchar(45) NOT NULL,
+  `PrimApe` varchar(45) NOT NULL,
+  `SegApe` varchar(45) NOT NULL,
+  `IdMedico` int NOT NULL,
+  `IdTratamiento` int NOT NULL,
+  `IdTipoSangre` int NOT NULL,
+  `IdAlergias` int NOT NULL,
+  `Correo` varchar(45) NOT NULL,
+  `Direccion` varchar(45) NOT NULL,
+  `DUI` int NOT NULL,
+  `ContEm` varchar(45) NOT NULL,
+  `FechaNac` datetime NOT NULL,
+  `Edad` int NOT NULL,
+  PRIMARY KEY (`idPaciente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paciente`
+--
+
+LOCK TABLES `paciente` WRITE;
+/*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tiposangre`
+--
+
+DROP TABLE IF EXISTS `tiposangre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tiposangre` (
+  `IdTipoSangre` int NOT NULL AUTO_INCREMENT,
+  `TipoSangre` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdTipoSangre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tiposangre`
+--
+
+LOCK TABLES `tiposangre` WRITE;
+/*!40000 ALTER TABLE `tiposangre` DISABLE KEYS */;
+INSERT INTO `tiposangre` VALUES (1,'A+'),(2,'D+');
+/*!40000 ALTER TABLE `tiposangre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tratamientos`
+--
+
+DROP TABLE IF EXISTS `tratamientos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tratamientos` (
+  `IdTratamientos` int NOT NULL AUTO_INCREMENT,
+  `Tratamiento` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdTratamientos`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tratamientos`
+--
+
+LOCK TABLES `tratamientos` WRITE;
+/*!40000 ALTER TABLE `tratamientos` DISABLE KEYS */;
+INSERT INTO `tratamientos` VALUES (1,'Extraccion');
+/*!40000 ALTER TABLE `tratamientos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-31 22:13:55
